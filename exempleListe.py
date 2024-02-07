cc9e031f39cc7cd8cd20628deb3ee2559c3002c8
Elias JawarnyPi <Jawarny@hotmail.fr>
2024-02-07 11:35:42 -0500

    Ajout d'exempleListe

---
+++ exempleListe.py
@@ -0,0 +1,19 @@
+
+language = list("c#", "python", "java", "sql")
+nombres = list([45,76,89,-1,23])
+nombres2 = list([45,76,89,-1,23])
+
+print ("parcourir avec boucle while")
+[print(i) for i in language]
+
+#boucle while 
+print("parcourir avec boucle while")
+ctrl = 0
+while ctrl < len(language):
+    print(language[ctrl])
+    ctrl+=1 # ou ctrl = ctrl + 1
+
+# ordre croissant
+print ("avant tri : ", nombres2)
+nombres2.sort()
+print ("apres tri : ", nombres2)
