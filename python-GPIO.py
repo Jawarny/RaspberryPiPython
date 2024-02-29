31959cedb15ba62bcba8249d2f661a00aa1797c9
Jawarny <jawarny@hotmail.fr>
2024-02-29 10:23:04 -0500

    Faire fonctionner l'incrementation de lumière 0.01

--- python-GPIO.py
+++ python-GPIO.py
@@ -16,31 +16,70 @@ ledJaune = PWMLED(13)
 ledRouge = PWMLED(19)
 ledVert = PWMLED(26)
 
-def augmenterLaLuminosite(led):
-    if (led.value <= 0.9):
-        led.value += 0.1
-    else:
-        led.value = 1
+def augmenterLaLuminosite(led, valeur, utiliserValeur):
+    if valeurMax(led):
+        print("valeur plus petite que 1")
+        if utiliserValeur:
+            print("incremente")
+            print("la valeur est :" + str(led.value))
+            if led.value == 0.06:
+                print("essai")
+                led.value = 0.07
+            if led.value == 0.57:
+                print("essai")
+                led.value = 0.581
+            else:
+                led.value += valeur
+        else:
+            print("met 1")
+            led.value = valeur
 
-def diminuerLaLuminosite(led):
-    if (led.value >= 0.1):
-        led.value -= 0.1
-    else:
-        led.value = 0
+def diminuerLaLuminosite(led, valeur, utiliserValeur):
+    if valeurMin(led):
+        print("valeur plus grande que 0")
+        if utiliserValeur:
+            print("decremente")
+            led.value -= valeur
+        else:
+            print("met 1")
+            led.value = valeur
+
+def valeurMin(led):
+    return (led.value > 0)
+
+def valeurMax(led):
+    return (led.value < 1)
+
+
 
 
 
 
 while True:
+    #input("1) Augmenter\n2) Allumer")
     if bouttonJaune.is_pressed:
-        augmenterLaLuminosite(ledJaune)
-        sleep(0.2)
+        if ledJaune.value <= 0.99999 and ledRouge.value >= 0.99999 and ledVert.value >= 0.99999:
+            augmenterLaLuminosite(ledJaune, 1, False)
+            augmenterLaLuminosite(ledRouge, 1, False)
+            augmenterLaLuminosite(ledVert, 1, False)  
+            sleep(0.2) 
+            print(str(ledJaune.value) + "\n" + str(ledRouge.value )+ "\n" + str(ledVert.value)) 
+            print("valeur pas 1")    
+        
+            
+       
     
     if bouttonRouge.is_pressed:
-        diminuerLaLuminosite(ledJaune)
+        diminuerLaLuminosite(ledJaune, 0.01, True)
+        diminuerLaLuminosite(ledRouge, 0.01, True)
+        diminuerLaLuminosite(ledVert, 0.01, True)
         sleep(0.2)
+        print(str(ledJaune.value) + "\n" + str(ledRouge.value )+ "\n" + str(ledVert.value)) 
         
     if bouttonVert.is_pressed:
-        print("boutton vert est pressed")
+        augmenterLaLuminosite(ledJaune, 0.01, True)
+        augmenterLaLuminosite(ledRouge, 0.01, True)
+        augmenterLaLuminosite(ledVert, 0.01, True)
         sleep(0.2)
+        print(str(ledJaune.value) + "\n" + str(ledRouge.value )+ "\n" + str(ledVert.value)) 
         
